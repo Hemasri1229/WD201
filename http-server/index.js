16f3ee2cf1206e631159d7149940a67fcb79912f
sairamalakshmi <diddisairamalakshmi@gmail.com>
2025-05-18 08:50:38 +0000

    Add registration route, HTML files, and port config via minimist

---
+++ http-server/index.js
@@ -0,0 +1,49 @@
+const http = require("http");
+const fs = require("fs");
+const minimist = require("minimist");
+
+const args = minimist(process.argv.slice(2));
+const port = args.port || 3000;
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+
+// Read HTML files
+fs.readFile("home.html", (err, home) => {
+  if (err) throw err;
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) throw err;
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, registration) => {
+  if (err) throw err;
+  registrationContent = registration;
+});
+
+http
+  .createServer((request, response) => {
+    const url = request.url;
+    response.writeHead(200, { "Content-Type": "text/html" });
+  switch(url){
+    case "/registration":
+      response.write(registrationContent);
+      response.end();
+      break;
+    case "/project":
+      response.write(projectContent);
+      response.end();
+      break;
+    default:
+      response.write(homeContent);
+      response.end();
+      break;
+    }
+  })
+  .listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
